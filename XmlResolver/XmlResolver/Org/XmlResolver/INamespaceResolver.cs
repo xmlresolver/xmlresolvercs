497dc299a00df339555ffe1f6a9839f44f82a9a1
Norman Walsh <ndw@nwalsh.com>
2021-12-02 15:29:33 +0000

    Make INamespaceResolver public

--- XmlResolver/XmlResolver/Org/XmlResolver/INamespaceResolver.cs
+++ XmlResolver/XmlResolver/Org/XmlResolver/INamespaceResolver.cs
@@ -2,7 +2,7 @@ using System;
 
 #nullable enable
 namespace Org.XmlResolver {
-    interface INamespaceResolver {
+    public interface INamespaceResolver {
         public object? GetEntity(Uri absoluteUri, string nature, string purpose);
         public object? GetEntity(String href, Uri baseUri, string nature, string purpose);
     }
