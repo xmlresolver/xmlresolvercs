68fb6f455eb48d94ca660b89d58da0bdd7d09187
Norman Walsh <ndw@nwalsh.com>
2021-12-10 10:07:36 +0000

    Documentation comments

--- XmlResolver/XmlResolver/Org/XmlResolver/Utils/PublicId.cs
+++ XmlResolver/XmlResolver/Org/XmlResolver/Utils/PublicId.cs
@@ -1,11 +1,22 @@
 using System;
 
 namespace Org.XmlResolver.Utils {
+    /// <summary>
+    /// Performs RFC 3151 encoding and decoding of public identifier and URNs.
+    /// </summary>
     public class PublicId {
         private PublicId() {
             // you can't make one of these.
         }
 
+        /// <summary>
+        /// Normalizes whitespace in a public identifier.
+        /// </summary>
+        /// <para>Whitespace normalization replaces tabs, new lines, and carriage returns with spaces,
+        /// trims off leading and trailing whitespace, and replaces all occurrences of more than one
+        /// consecutive space with a single space.</para>
+        /// <param name="publicId">The public identifier.</param>
+        /// <returns>The normalized identifier.</returns>
         public static string Normalize(string publicId) {
             string normal = publicId.Replace('\t', ' ');
             normal = normal.Replace('\r', ' ');
@@ -21,7 +32,12 @@ namespace Org.XmlResolver.Utils {
             return normal;
        }
         
-        
+        /// <summary>
+        /// Encodes a public identifier.
+        /// </summary>
+        /// <para>Performs RFC 3151 encoding of a public identifier to yield a URN.</para>
+        /// <param name="publicId">The public identifier.</param>
+        /// <returns>A <c>urn:publicid:</c> URN.</returns>
         public static Uri EncodeUrn(string publicId) {
             string urn = PublicId.Normalize(publicId)
                 .Replace("%", "%25")
@@ -39,6 +55,14 @@ namespace Org.XmlResolver.Utils {
             return new Uri("urn:publicid:" + urn);
         }
 
+        /// <summary>
+        /// Decodes a <c>urn:publicid:</c> URN into a public identifier.
+        /// </summary>
+        /// <para>Performs RFC 3151 decoding of a URN to yield a public identifier.</para>
+        /// <para>If the specified <c>urn</c> does not appear to be a
+        /// <c>urn:publicid:</c> URN, it is returned unchanged.</para>
+        /// <param name="urn">The <c>urn:publicid:</c> URN.</param>
+        /// <returns>The public identifier.</returns>
         public static string DecodeUrn(string urn) {
             if (urn.StartsWith("urn:publicid:")) {
                 string publicId = urn.Substring(13)
